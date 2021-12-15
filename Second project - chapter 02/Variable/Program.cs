39da1cf81f148f949663f58b9f68884d0630c573
heitormarinha <heitor.mpr@gmail.com>
2021-12-15 14:57:07 -0300

    commit

--- Second project - chapter 02/Variable/Program.cs
+++ Second project - chapter 02/Variable/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿// Storing any type of object
+object height = 1.88; // Storing a douible in an object
+object name = "Amir"; // Storing a string in an object
+Console.WriteLine($"{name} is {height} metres tall."); // String interpoled
