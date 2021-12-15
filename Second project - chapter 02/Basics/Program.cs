1b884ee3f612037b002359484942499401bc98b4
heitormarinha <heitor.mpr@gmail.com>
2021-12-15 13:33:21 -0300

    commit

---
+++ Second project - chapter 02/Basics/Program.cs
@@ -0,0 +1,15 @@
+﻿// #error version
+using System; // a semicolon indicates the end of a statement
+
+namespace Basics
+{ // A open brace indicates the start of a block
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double heighInMetres = 1.88;
+           // See https://aka.ms/new-console-template for more information
+            Console.WriteLine($"The variable {nameof(heighInMetres)} has a value {heighInMetres}");
+        }
+    }
+} // A close brace indicates the end of a block
---
+++ Second project - chapter 02/Number/Program.cs
@@ -0,0 +1,24 @@
+﻿
+/*
+// Unsigned integer means positive whole number, and this including 0.
+uint naturalNumber = 23;
+
+// Integer means negative or positive whole number, and this including 0.
+int integerNumber = -23;
+
+// Float means single-precision floating point, and need a F suffix makes it a float literal.
+float realNumber = 2.3F;
+
+// Double means double-precision floating pont.
+double anotherRealNumber = 2.3;// Double literal
+*/
+
+// Three variables that store the number 2 million
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+// Now we are checking if the three variables have the same value.
+// Both the following output of the statements are "true".
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine(   $"{decimalNotation == hexadecimalNotation}");
