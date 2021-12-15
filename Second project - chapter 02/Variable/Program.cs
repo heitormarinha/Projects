f861c637eaf7ff10183931d2084a05c420025777
heitormarinha <heitor.mpr@gmail.com>
2021-12-15 17:02:45 -0300

    commit

--- Second project - chapter 02/Variable/Program.cs
+++ Second project - chapter 02/Variable/Program.cs
@@ -11,4 +11,28 @@ Console.WriteLine($"{name} has {length2} characters."); // It is a interpoled st
 dynamic anotherName = "Ahmed";
 
 // This compiles, but would throw an exception at run-time and if you later store a data type which does not have a property named lenght
-int lenght = anotherName.Lenght;
+int length = anotherName.Length;
+
+// Specifying and inferring the type of a local variable - (camelCasing)
+// Declaring and assign values to some local variables using specific types
+/*int population = 66_000_000; // 6 milion in UK
+double weight = 1.88; // In Kilogram
+decimal price = 4.99M; // In pounds sterling
+string fruit = "apples"; // String use double-quotes
+char letter = 'Z'; // Chars use single-quotes
+bool happy = true; // Booleans have value of true or false*/
+
+// Specifying and inferring the type of a local variable - (camelCasing)
+// Declaring and assign values to some local variables without specific types, so using var keyword
+/**var population = 66_000_000; // 66 million in UK  
+var weight = 1.88; // in kilograms 
+var price = 4.99M; // in pounds sterling 
+var fruit = "Apples"; // strings use double-quotes  
+var letter = 'Z'; // chars use single-quotes 
+var happy = true; // Booleans have value of true or false*/
+
+// Geeting the default values of the local variable
+Console.WriteLine($"default(int) = {default(int)}.");
+Console.WriteLine($"default(bool) = {default(bool)}");
+Console.WriteLine(   $"default(DateTime) = {default(DateTime)}");
+Console.WriteLine(   $"default(string) = {default(string)}");
