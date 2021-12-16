2d3ba850a1b81853db72389dace4cda64b5bffe8
heitormarinha <heitor.mpr@gmail.com>
2021-12-16 16:30:27 -0300

    commit

--- Second project - chapter 02/Formatting/Program.cs
+++ Second project - chapter 02/Formatting/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+
+// Using format method of string
+// EG1:
+Console.WriteLine(
+    format: "{0} apples costs {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+
+// EG2:
+Console.WriteLine(
+    format: "{0} apples cost {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+
