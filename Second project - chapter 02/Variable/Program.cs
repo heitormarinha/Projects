69a0fa47d1315cb446871f2a2600d4699b2c90bf
HEITOR MOTTA PIRES DOS REIS <74876769+heitormarinha@users.noreply.github.com>
2021-12-15 22:09:38 -0300

    Update Program.cs

--- Second project - chapter 02/Variable/Program.cs
+++ Second project - chapter 02/Variable/Program.cs
@@ -36,3 +36,4 @@ Console.WriteLine($"default(int) = {default(int)}.");
 Console.WriteLine($"default(bool) = {default(bool)}");
 Console.WriteLine(   $"default(DateTime) = {default(DateTime)}");
 Console.WriteLine(   $"default(string) = {default(string)}");
+//test
