6e72ba5dd3893b3cd96d1a90eac93c4a22fe23ee
heitormarinha <heitor.mpr@gmail.com>
2021-12-16 15:48:56 -0300

    commit

--- Second project - chapter 02/NullHandling/Program.cs
+++ Second project - chapter 02/NullHandling/Program.cs
@@ -9,3 +9,25 @@ Console.WriteLine(thisCouldtBeNull.GetValueOrDefault());
 thisCouldtBeNull = 7;
 Console.WriteLine(thisCouldtBeNull);
 Console.WriteLine(thisCouldtBeNull.GetValueOrDefault());
+
+// Check that the variable is not null before using it
+if (thisCouldtBeNull != null)
+{
+    // acess a member of thisCoudBeNull
+    // int length = (thisCouldtBeNull).Length; // Could throw exception cause the int does not have the Length member
+
+}
+
+// If you are trying to use a member of a variable that might be null, use the null-conditional  operator "?"
+
+string authorName = null;
+
+// The following throws the ID of type int special name a NullReferenceException
+int x = authorName.Length; //
+
+// Instead of throwing an exception, null is assigned to y
+int? y = authorName?.Length; // the suffix "?" let the null be assingned.
+
+// result will be 3 if authorName?.Length is null 
+var result = authorName?.Length ?? 3;  
+Console.WriteLine(result);
