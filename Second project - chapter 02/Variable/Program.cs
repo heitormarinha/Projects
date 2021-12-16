efc964b705bd1c387c193b4e1e2f450b099c03fa
heitormarinha <heitor.mpr@gmail.com>
2021-12-15 22:22:01 -0300

    commit

--- Second project - chapter 02/Variable/Program.cs
+++ Second project - chapter 02/Variable/Program.cs
@@ -36,4 +36,3 @@ Console.WriteLine($"default(int) = {default(int)}.");
 Console.WriteLine($"default(bool) = {default(bool)}");
 Console.WriteLine(   $"default(DateTime) = {default(DateTime)}");
 Console.WriteLine(   $"default(string) = {default(string)}");
-
