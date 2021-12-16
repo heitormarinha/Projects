bc94d49aa219a149ee1c5eef0a29c1ef8a8063c8
heitormarinha <heitor.mpr@gmail.com>
2021-12-16 17:06:52 -0300

    commit

--- Second project - chapter 02/Formatting/Program.cs
+++ Second project - chapter 02/Formatting/Program.cs
@@ -14,3 +14,36 @@ Console.WriteLine(
     arg0: numberOfApples,
     arg1: pricePerApple * numberOfApples);
 
+string applesText = "Apples";
+int applesCount = 1234;
+string bananasText = "Bananas";
+int bananasCount =56789;
+
+Console.WriteLine(
+    format: "{0,-8} {1,6:N0}",
+    arg0: "Name",
+    arg1: "Count");
+
+Console.WriteLine(   
+    format: "{0,-8} {1,6:N0}",
+    arg0: applesText,
+    arg1: applesCount);
+
+Console.WriteLine(   
+    format: "{0,-8} {1,6:N0}",
+    arg0: bananasText,
+    arg1: bananasCount);
+
+Console.Write("Type your first name and press ENTER: ");
+// Getting the text inpit form user using the "ReadLine method" of system.console
+string firstName = Console.ReadLine();
+
+Console.Write("Type your age and press ENTER: ");
+uint age = Console.ReadLine();
+
+// Console.WriteLine(   $"Hello {firstName}, you look good for {age}.");
+
+Console.WriteLine(
+    format:"Hello {0}, you look good for {1}!",
+    arg0: firstName,
+    arg1: age);
