6a42d42b7e97f726b0fa5cded9c3cf01222a046c
heitormarinha <heitor.mpr@gmail.com>
2021-12-17 11:33:58 -0300

    commit

---
+++ Second project - chapter 02/Arguments/Program.cs
@@ -0,0 +1,19 @@
+﻿// Importing the type "Console" in the namespace named system.
+using static System.Console;
+
+namespace Arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Printing the length or args
+            WriteLine($"there are {args.Length} arguments.");
+            // Printing the args
+            foreach (string arg in args) // args in the arg length
+            {
+                WriteLine(arg);
+            }
+        }
+    }
+}
