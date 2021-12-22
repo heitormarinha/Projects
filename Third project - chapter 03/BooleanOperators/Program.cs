08865235fa76b7597aee51789539c74bacd0023f
heitormarinha <heitor.mpr@gmail.com>
2021-12-21 22:47:32 -0300

    commit

--- Third project - chapter 03/BooleanOperators/Program.cs
+++ Third project - chapter 03/BooleanOperators/Program.cs
@@ -1,6 +1,18 @@
 ﻿// statically importing the System.Console  type to simplify statements in a console app.
-using statically System.Console;
+using static System.Console;
 
-// Declaring type in explicit mode
-
+// Declaring with a specifc type
+bool a = true;
+bool b = false;
+WriteLine($"AND | a     | b ");
+WriteLine($"a   | {a & a,-5} | {a & b,-5} ");
+WriteLine($"b   | {b & a,-5} | {b & b,-5} ");
+WriteLine();
+WriteLine($"OR  | a     | b ");
+WriteLine($"a   | {a | a,-5} | {a | b,-5} ");
+WriteLine($"b   | {b | a,-5} | {b | b,-5} ");
+WriteLine();
+WriteLine($"XOR | a     | b ");
+WriteLine($"a   | {a ^ a,-5} | {a ^ b,-5} ");
+WriteLine($"b   | {b ^ a,-5} | {b ^ b,-5} ");
 
