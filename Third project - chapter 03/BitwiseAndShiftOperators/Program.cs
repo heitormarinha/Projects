6f6a3f2e52b91b397d00cbc3e934b0f44a1653ea
heitormarinha <heitor.mpr@gmail.com>
2021-12-21 23:02:19 -0300

    commit

---
+++ Third project - chapter 03/BitwiseAndShiftOperators/Program.cs
@@ -0,0 +1,7 @@
+﻿// Importing in static mode using the namespace "using" the namespace "System" and its "Console" type.
+using static System.Console;
+
+int a = 10; // 0000 1010
+int b = 6; // 0000 0110
+
+Wri
