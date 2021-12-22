c94319e82c8bcdb882f9dc19458bf2a53bb4b529
heitormarinha <heitor.mpr@gmail.com>
2021-12-22 14:28:17 -0300

    commit

--- Third project - chapter 03/BitwiseAndShiftOperators/Program.cs
+++ Third project - chapter 03/BitwiseAndShiftOperators/Program.cs
@@ -1,7 +1,17 @@
 ﻿// Importing in static mode using the namespace "using" the namespace "System" and its "Console" type.
 using static System.Console;
 
-int a = 10; // 0000 1010
-int b = 6; // 0000 0110
+int a = 10; // Variable "a" will be assigned with the literal valule 10 (0000 1010 in binary).
 
-Wri
+int b = 6; // // Variable "b" will be assigned with the literal valule 10 (0000 0110 in binary).
+
+WriteLine($"a = {a}"); 
+WriteLine($"b = {b}");
+
+// These operations happens in binary 
+WriteLine($"a & b = {a & b}"); // 2-bit column only.
+WriteLine($"a | b = {a | b}"); // 8, 4, and 2-bit columns.
+WriteLine($"a ^ b = {a ^ b}"); // 8 and 4-bit columns.
+
+
+
