bcb0de615e42858f5436d981b996c48d8883cd46
heitormarinha <heitor.mpr@gmail.com>
2021-12-22 21:24:17 -0300

    commit

--- Third project - chapter 03/HandlingExceptions/Program.cs
+++ Third project - chapter 03/HandlingExceptions/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Importing the System type and its Console type in static mode.
+using static System.Console;
+
+namespace HandlingException
+{
+    class program
+    {
+        static void Main()
+        {
+            WriteLine("Before Parsing");
+            WriteLine("What is your age?");
+            string? input = ReadLine();
+
+            try
+            {
+                int? age = int.Parse(input);
+                WriteLine($"You are {age} years old.");
+            }
+            catch(OverflowException)
+            {
+                WriteLine("Your age is a valid number format but it is either too big or  small.");
+            }
+            catch(FormatException)
+            {
+                WriteLine("The age tha you entered in not a valid number format ");
+            }
+            WriteLine("After parsing");
+
+
+
+
+
+        } //End of Main method.
+    } // End of Program Class.
+} // End of namespace HandlingExceotions.
