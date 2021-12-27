935276e0d655b8aeb8f11980e353b6e841f9237c
heitormarinha <heitor.mpr@gmail.com>
2021-12-27 14:25:36 -0300

    commit

--- Fourth project - Chapter 04/WritingFunctions/Program.cs
+++ Fourth project - Chapter 04/WritingFunctions/Program.cs
@@ -1,18 +1,21 @@
 ﻿// Importing in "static" mode the namespace System type and its Console type
 using static System.Console;
 
-namespace WritingFunctions 
+namespace WritingFunctions // Declaring a name space.
 {   
-    class Program 
+    class Program // Declaring a Class
     {
 
         // The method is declared above/before the main method.
         
-        static void TimesTable(byte number) // Creating a method that handle one argumet in byte type in static mode. 
+        static void TimesTable(byte number) // Creating a method that handle one argumet in byte type in static mode and with tha ID name TimesTable.
         {
 
-            WriteLine("This is the {numer} times table:");// Literal string .
-            for (int row = 1; row <= 12; row++) // Iteration with for.
+            WriteLine($"This is the {number} times table:");// Interpoled string .
+
+            // Starting a iteration statements (aka loop.)
+            // An initializer (which executes once at the start of the loop.), a conditional(Which executes on everu interation at the start of the loop to check wheteher the looping should continue) and a iterator expressions(which executes on everu loop at the botton of the statement).
+            for (int row = 1; row <= 12; row++) // Iteration(loop) with for.
             {
                 WriteLine(
                     $"{row} x {number} = {row * number}"
@@ -23,7 +26,7 @@ namespace WritingFunctions
         } // End of TimesTable method.
 
         // Creating other method/funciton.
-        static void RunTimesTable() // Making a function 
+        static void RunTimesTable() // Making a function in static mode and void (without a type).
         {
             bool isNumber;
             do
@@ -40,8 +43,8 @@ namespace WritingFunctions
                      WriteLine("You did not enter a valid number!");
                 }      
             } // End of block of iteration do.      
-            while (isNumber); // End of iterarion DO.        
-        }
+            while (isNumber); // End of iterarion with DO.        
+        } // End of RunTimesTable function.
         static void Main(string[] args)
         {
             RunTimesTable();
