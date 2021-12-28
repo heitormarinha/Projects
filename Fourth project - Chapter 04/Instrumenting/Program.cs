faafa5005474faa85e786c54ff150f8fdaf74250
heitormarinha <heitor.mpr@gmail.com>
2021-12-28 19:39:43 -0300

    commit

--- Fourth project - Chapter 04/Instrumenting/Program.cs
+++ Fourth project - Chapter 04/Instrumenting/Program.cs
@@ -3,6 +3,9 @@ using System.Diagnostics;
 // Importing the System type and its IO type.
 using System.IO;
 
+//I am importing the Microsoft.Extensions.Configuration namespace.
+using Microsoft.Extensions.Configuration;
+
 // I am writiting the namespace Instrumenting.
 namespace Instrumenting
 {
@@ -22,7 +25,28 @@ namespace Instrumenting
             // Printing the output with Debug and Trace type in literal string.            
             Debug.WriteLine("Debug says, I am watching!");
             Trace.WriteLine("Trace says, I am watching");  
-        }
 
-    }
-}
+            // I am declaring a variable with ID name builder with ConfigurationBuilder type in inference mode(var).
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("AppSettings.json", optional: true, reloadOnChange:true);
+            // The builder has the type ConfigurationBuilder and it has the preview setting.
+
+            // I am Declaring the ID named configuration variable with the IConfigurationRoot type.
+            IConfigurationRoot configuration = builder.Build();
+
+            // I am declaring the variable in inference mode with TraceSwitch type and with the ID name ts(traceSwitch)
+            var traceSwitch = new TraceSwitch
+            (
+                displayName: "PackSwitch",
+                description: "This switch is set via a JSON config."
+            );
+            
+            configuration.GetSection("PackSwitch").Bind(traceSwitch);
+            Trace.WriteLineIf(traceSwitch.TraceError, "Trace error");
+            Trace.WriteLineIf(traceSwitch.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(traceSwitch.TraceInfo, "Trace information");
+            Trace.WriteLineIf(traceSwitch.TraceVerbose, "Trace verbose");
+
+
+        } // End of Main method.
+    } // End of Class Program.
+} // End of name space.
