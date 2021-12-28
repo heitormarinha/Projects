2d451dbfaf920c350065f3e1efd775b0465e2f50
heitormarinha <heitor.mpr@gmail.com>
2021-12-28 18:39:23 -0300

    commit

---
+++ Fourth project - Chapter 04/Instrumenting/Program.cs
@@ -0,0 +1,18 @@
+﻿// Importing the System type (domain) and its Diagnostics type.
+using System.Diagnostics;
+
+// I am Writiting the namespace Instrumenting.
+namespace Instrumenting
+{
+    //I am Writing a class with Id name Program
+    class Program
+    {
+        // I am Writing in static mode without type (void) the ID name Main method/function and its parameter in string type with ID name args.
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Debug says, I am watching!");
+            
+        }
+
+    }
+}
