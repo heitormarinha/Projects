705b3ec5732ae8f08e2c8a682cceebb0b3394f0f
heitormarinha <heitor.mpr@gmail.com>
2021-12-29 20:38:56 -0300

    commit

--- Fifth project - chapter 05/PeopleApp/Program.cs
+++ Fifth project - chapter 05/PeopleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Importing (using) the library namespaces ID name System.Console and Packt.Shared
+// Obs: the System.Console will be imported in static.
+using static System.Console;
+using Packt.Shared;
+//I am writing a namespace with ID name PeopleApp.
+namespace PeopleApp
+{
+    // I am writing a class with ID name Program.
+    class Program
+    {
+        // I am writing in static mode, without type  the Main method.
+        static void Main()
+        {
+            var bob = new Person();
+            WriteLine(bob.ToString());
+        }
+    }
+}
