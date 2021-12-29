3b318f424265cbfaaca246fb1675b6d9d8908c28
heitormarinha <heitor.mpr@gmail.com>
2021-12-29 15:01:39 -0300

    commit

---
+++ Fourth project - Chapter 04/FactorialNumber/FactorialLibUnitTest/FactorialLibUnitTest.cs
@@ -0,0 +1,23 @@
+using Xunit;
+using FactorialPackt;
+
+namespace FactorialLibUnitTest;
+
+public class FactorialLibUnitTest
+{
+    [Fact]
+    public void testFactorialCalculation()
+    {
+        // Arrange.
+        // int a = 7;
+       // int expected = 7;
+        //var factorialCalculation = new Factorial();
+
+        // Act.
+        //int actual = factorialCalculation.FactorialCalculation(a);
+
+        // Assert.
+        //Assert.Equal(expected, actual);
+
+    }
+}
--- Fourth project - Chapter 04/FactorialNumber/Program.cs
+++ Fourth project - Chapter 04/FactorialNumber/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// I am importing in static mode the System type and its Console.
+using static System.Console;
+
+// I am importing the FactorialPackt library.
+using FactorialPackt;
+
+// I am writing the namescace ID name FactorialNumber.
+namespace FactorialNumber
+{
+    class Program
+    {
+        // Writing in static mode and without type the Main function.
+        static void Main()
+        {
+            int number = 4;
+            new Factorial().FactorialCalculation(number);
+        }
+    }
+}
