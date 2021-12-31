5830ce7bc03d2b4832a164603bf80a8261b96072
heitormarinha <heitor.mpr@gmail.com>
2021-12-31 16:20:31 -0300

    commit

---
+++ Fifth project - chapter 05/PacktLibrary9/FlightPatterns.cs
@@ -0,0 +1,15 @@
+namespace Packt.Shared;
+public class BusinessClassPassenger
+{
+    public override string ToString() => $"Business Class"; 
+}
+public class FirstClassPassenger
+{
+    public int AirMiles { get; set;}
+    public override string ToString() => $"First Class with {AirMiles:N0} air miles";
+} 
+public class CoachClassPassenger
+{
+    public double CarryOnKG { get; set; }
+    public override string ToString() => $"Coach Class with {CarryOnKG:N2} KG carry on";
+}
